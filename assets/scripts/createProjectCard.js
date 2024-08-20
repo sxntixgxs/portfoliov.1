1fdb85f09aec6aa55b881ce4db8d28f470a0c617
Santiago Sandoval Torres <145622597+sxntixgxs@users.noreply.github.com>
2024-08-19 21:09:44 -0500

    first release

--- assets/scripts/createProjectCard.js
+++ assets/scripts/createProjectCard.js
@@ -1,13 +1,19 @@
 export function createProjectCard(project) {
     return `
-        <div class="project-card card rounded-lg shadow-lg p-6"">
-            <img src="${project.image}" alt="${project.title}" class="rounded-t-lg mb-4 object-cover h-64 w-full">
-            <h3 class="text-xl font-bold mb-2 title-style">${project.title}</h3>
-            <p class="desc mb-4">${project.description}</p>
-            <div class="technologies flex flex-wrap gap-2 justify-center">
-                ${project.technologies.map(tech => `<span class="tech-item-card p-3 rounded-lg shadow inline-flex items-center text-sm">${tech}</span>`).join('')}
+        <div class="project-card border border-white rounded-lg overflow-hidden shadow-lg transition-transform transform hover:scale-105">
+            <a href="${project.link}" target="_blank" class="block relative">
+                <img src="${project.image}" alt="${project.title}" class="w-full h-48 object-cover transition-opacity duration-300 hover:opacity-80">
+                <div class="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 opacity-0 hover:opacity-100 transition-opacity duration-300">
+                    <span class="text-white text-lg font-bold">${project.title}</span>
+                </div>
+            </a>
+            <div class="p-4">
+                <h3 class="text-xl font-bold">${project.title}</h3>
+                <p>${project.description}</p>
+                <div class="mt-2">
+                    ${project.technologies.map(tech => `<span class="btn-icon p-2 rounded-lg shadow inline-flex items-center text-sm hover:bg-600 hover:text-white-700 focus:outline-none focus:ring-2 focus:ring-green-300 ">${tech}</span>`).join('')}
+                </div>
             </div>
         </div>
     `;
 }
-
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,5 @@
 import { createProjectCard } from "./createProjectCard.js";
 
-
 document.addEventListener('DOMContentLoaded', function() {
     fetch('data/projects.json')
         .then(response => response.json())
@@ -8,5 +7,41 @@ document.addEventListener('DOMContentLoaded', function() {
             const container = document.getElementById('projects-container');
             container.innerHTML = projects.map(createProjectCard).join('');
         });
-});
 
+    const form = document.getElementById('contact-form');
+    const modal = document.getElementById('modal');
+    const modalCloseButton = document.getElementById('modal-close');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault(); // Prevenir el envío estándar del formulario
+
+        const formData = new FormData(form);
+
+        fetch(form.action, {
+            method: 'POST',
+            body: formData,
+            headers: {
+                'Accept': 'application/json'
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.ok) {
+                console.log("enviado enviado")
+                modal.classList.remove('hidden'); // Mostrar el modal
+                form.reset(); // Opcional: Limpiar el formulario después del envío
+            } else {
+                console.log("wtfff")
+                // Manejo de errores aquí si es necesario
+            }
+        })
+        .catch(error => {
+            // Manejo de errores aquí si es necesario
+        });
+    });
+
+    // Cerrar el modal
+    modalCloseButton.addEventListener('click', function() {
+        modal.classList.add('hidden'); // Ocultar el modal
+    });
+});
