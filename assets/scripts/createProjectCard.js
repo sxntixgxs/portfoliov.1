a57fdf89fd8b23ac381ef7a20bd130ad1d6f2ed8
Santiago Sandoval Torres <145622597+sxntixgxs@users.noreply.github.com>
2024-08-19 19:28:37 -0500

    mas o menos las tarjetas

--- assets/scripts/createProjectCard.js
+++ assets/scripts/createProjectCard.js
@@ -1,12 +1,13 @@
-export function createProjectCard(project){
+export function createProjectCard(project) {
     return `
-        <div class="project-card bg-white rounded-lg shadow-lg p-6">
-            <img src="${project.image}" alt="${project.title}" class="rounded-t-lg mb-4">
-            <h3 class="text-xl font-bold mb-2">${project.title}</h3>
-            <p class="text-gray-700 mb-4">${project.description}</p>
-            <div class="technologies flex flex-wrap gap-2">
-                ${project.technologies.map(tech=>`<span class="bg-gray-200 rounded-full px-3 py-1 text-sm text-gray-700">${tech}</span>`).join('')}
+        <div class="project-card card rounded-lg shadow-lg p-6"">
+            <img src="${project.image}" alt="${project.title}" class="rounded-t-lg mb-4 object-cover h-64 w-full">
+            <h3 class="text-xl font-bold mb-2 title-style">${project.title}</h3>
+            <p class="desc mb-4">${project.description}</p>
+            <div class="technologies flex flex-wrap gap-2 justify-center">
+                ${project.technologies.map(tech => `<span class="tech-item-card p-3 rounded-lg shadow inline-flex items-center text-sm">${tech}</span>`).join('')}
             </div>
         </div>
     `;
 }
+
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,10 +1,12 @@
 import { createProjectCard } from "./createProjectCard.js";
 
-document.addEventListener('DOMContentLoaded',function(){
+
+document.addEventListener('DOMContentLoaded', function() {
     fetch('data/projects.json')
         .then(response => response.json())
         .then(projects => {
             const container = document.getElementById('projects-container');
             container.innerHTML = projects.map(createProjectCard).join('');
-        })
-})
+        });
+});
+
